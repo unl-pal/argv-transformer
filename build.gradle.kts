67a4ac94076dd8c3e1eae53ed175612569356ce8
jckreg <jonathancraig@boisestate.edu>
2024-12-06 12:37:32 -0700

    resolving all dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,38 +10,41 @@ repositories {
     dirs("lib")
   }
   mavenCentral() // Replace with your preferred repository if needed
+  maven {
+    url = uri("https://repo.eclipse.org/content/groups/releases/")
+  }
 }
 
 dependencies {
-  implementation(files("lib/bcel-6.2.jar"))
-  implementation(files("lib/commons-csv-1.5.jar"))
-  implementation(files("lib/commons-io-2.16.1.jar"))
-  implementation(files("lib/eclipse.jdt.core.jar"))
-//  implementation(files("lib/jpf.jar"))
-  implementation(files("lib/org.eclipse.core.contenttype-3.4.200-v20140207-1251.jar"))
-  implementation(files("lib/org.eclipse.core.jobs-3.6.0-v20140424-0053.jar"))
-  implementation(files("lib/org.eclipse.core.resources_3.7.101.dist.jar"))
-  implementation(files("lib/org.eclipse.core.runtime-3.7.0.jar"))
-  implementation(files("lib/org.eclipse.equinox.common-3.6.0.v20100503.jar"))
-  implementation(files("lib/org.eclipse.equinox.preferences-3.5.200-v20140224-1527.jar"))
-  implementation(files("lib/org.eclipse.jdt.ui-6.12.2.jar"))
-  implementation(files("lib/org.eclipse.jface.jar"))
-  implementation(files("lib/org.eclipse.ltk.core.refactoring-6.12.2.jar"))
-  implementation(files("lib/org.eclipse.osgi-3.7.1.jar"))
-  implementation(files("lib/org.eclipse.text_3.5.0.jar"))
-  // implementation(files("lib/snakeyaml-2.0.jar"))
-  // implementation(files("lib/sootclasses-trunk-jar-with-dependencies.jar"))
-  // implementation("org.apache.commons:commons-csv:1.5")
-  // implementation("commons-io:commons-io:2.16.1")
+// only 1 files holding back the project
+  // implementation(files("lib/eclipse.jdt.core.jar"))
+  // implementation(files("lib/org.eclipse.core.contenttype-3.4.200-v20140207-1251.jar"))
+// faulty fetches cannot be run
+  // implementation("org.eclipse.jdt:org.eclipse.jdt.core:3.13.0")
+  // implementation("org.eclipse.jdt:org.eclipse.jdt.core:3.16.0")
+  // implementation("org.eclipse:org.eclipse.jdt.core:3.3.1.v_780_R33x")
+  implementation("org.eclipse.jdt:org.eclipse.jdt.core:3.10.0.v20140604-1726")
+  // implementation("org.eclipse.jdt:org.eclipse.jdt.core:3.10.0.v20140604-1726")
   // implementation("org.eclipse.jdt:org.eclipse.jdt.core:3.40.0")
-  // implementation("org.eclipse.platform:org.eclipse.core.jobs:3.8.0")
-  // implementation("org.eclipse.core:org.eclipse.core.resources:3.7.100") // not the same
-  // implementation("org.eclipse.core:org.eclipse.core.runtime:3.7.0")
-  // implementation("org.eclipse.equinox:org.eclipse.equinox.common:3.6.0.v20100503")
-  // implementation("org.eclipse.platform:org.eclipse.equinox.preferences:3.6.1") // not the same
+  // implementation("org.eclipse.jdt:org.eclipse.jdt.ui:3.33.200")
+  // implementation("org.eclipse.platform:org.eclipse.swt:3.128.0")
+  // compileOnly("org.eclipse.swt:org.eclipse.swt.gtk.linux.x86_64:4.3")
+  // implementation("org.eclipse.osgi:org.eclipse.osgi")
+  // implementation("org.eclipse.jdt:org.eclipse.jdt.core:3.40.0")
+  // implementation("org.eclipse.jdt:org.eclipse.jdt.core:3.10.0")
+  // implementation("org.eclipse.jdt:org.eclipse.jdt.core:3.7.1")
   // implementation("org.eclipse.platform:org.eclipse.ltk.core.refactoring:3.14.600") // not the same
-  // implementation("org.eclipse.osgi:org.eclipse.osgi:3.7.1")
-  // implementation("org.eclipse.text:org.eclipse.text:3.5.101") // not the same
+// all other fetches work
+  implementation("org.apache.commons:commons-csv:1.5")
+  implementation("commons-io:commons-io:2.16.1")
+  implementation("org.eclipse.core:contenttype:3.4.200-v20140207-1251")
+  implementation("org.eclipse.core:org.eclipse.core.resources:3.7.100") // not the same
+  implementation("org.eclipse.core:org.eclipse.core.runtime:3.7.0")
+  implementation("org.eclipse.equinox:org.eclipse.equinox.common:3.6.0.v20100503")
+  implementation("org.eclipse.platform:org.eclipse.core.jobs:3.8.0")
+  implementation("org.eclipse.platform:org.eclipse.equinox.preferences:3.6.1") // not the same
+  implementation("org.eclipse.osgi:org.eclipse.osgi:3.7.1")
+  implementation("org.eclipse.text:org.eclipse.text:3.5.101") // not the same
   implementation("org.yaml:snakeyaml:2.0")
   implementation("org.soot-oss:soot:4.6.0")
   testImplementation("org.mockito:mockito-core:5.14.2")

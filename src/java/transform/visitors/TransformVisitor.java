f5d105e3e973e9d8b8184416fe01a94d30188d84
10625639 <cmoloney2@huskers.unl.edu>
2025-09-17 13:04:15 -0500

    Removing unncessary logging

--- src/java/transform/visitors/TransformVisitor.java
+++ src/java/transform/visitors/TransformVisitor.java
@@ -668,9 +668,6 @@ public class TransformVisitor extends ASTVisitor {
 	@Override
 	public void endVisit(MethodInvocation node) {
 		IMethodBinding methodBinding = node.resolveMethodBinding();
-		if (methodBinding != null) {
-		    System.out.println("Resolved binding for " + methodBinding.getDeclaringClass().getName());
-		}
         if (methodBinding != null && methodBinding.getDeclaringClass() != null) {
             String packageName = methodBinding.getDeclaringClass().getPackage().getName();
             if (rootNodePackage == null) {

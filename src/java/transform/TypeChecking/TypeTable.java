9c644a14280eaac91b1d4aec6d90f77180b04572
10625639 <cmoloney2@huskers.unl.edu>
2025-11-07 14:50:09 -0600

    Correcting issues with qualifiedname resolution

--- src/java/transform/TypeChecking/TypeTable.java
+++ src/java/transform/TypeChecking/TypeTable.java
@@ -29,6 +29,11 @@ public class TypeTable {
 	 * @return Type
 	 */
 	public Type getNodeType(ASTNode node) {
+	    if (typeTable.containsKey(node)) {
+	        System.out.println("Key exists for " + node.toString() + "!");
+	    } else {
+		    System.out.println("Key does not exist for " + node.toString() + "!");
+	    }
 		return typeTable.get(node);
 	}
 	

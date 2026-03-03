e97676b7265a02a71baa144b3dff51f544329408
10625639 <cmoloney2@huskers.unl.edu>
2026-03-03 16:38:39 -0600

    Correcting names and properties

--- src/java/transform/Main.java
+++ src/java/transform/Main.java
@@ -58,11 +58,11 @@ public class Main {
 	private static boolean debug = false;
 	private static boolean transformAll = false;
 
-//	public static String source = "database";
-//	public static String dest = "benchmarks";
+	public static String source = "suitablePrgms";
+	public static String dest = "benchmarks";
 //	 public static String source = "src/test/transformer/integration";
-	 public static String source = "testsFromReport";
-	 public static String dest = "testOutput";
+//	 public static String source = "testsFromReport";
+//	 public static String dest = "testOutput";
 
 	 public static void main(String[] args) throws IOException {
 	     File tmpDir = Files.createTempDirectory("paclab-transform").toFile();
